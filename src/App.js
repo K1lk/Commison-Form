9ee5a2cc3f8ad09faa1776fe06f6fe4d0e00b122
K1lk <gamingpro1567@gmail.com>
2023-01-05 19:43:17 -0800

    Adding it properly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+function CommisionStat() {
+return (
+<div id="Commision-Status">OPEN</div>
+)
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="App">
+	<header className="App-header">
+		<div className="Top-Container">
+			{/*Contains Logo*/}
+			<div className="Logo-Container">
+				<h1 className="Page-Title">
+					<img className="Logo" src="urmom.png" />
+					Kilk's Wacky Page
+				</h1>
+			</div>
+			{/*Contains Dropa Down*/}
+			<div className="Dropdown-Container">
+				<button className="Drop-Btn">
+					PlaceHolder
+				</button>
+				<div className="Dropdown-Content">
+					<a href="#">Link 1</a>
+					<a href="#">Link 2</a>
+					<a href="#">Link 3</a>
+				</div>
+			</div>
+		</div>
+		{/*Page Contents*/}
+		<div className="Page-Contents">
+			<h2>
+				VRC COMMISIONS ARE
+				<CommisionStat></CommisionStat>
+			</h2>
+			<h3>
+				Welcome to my homepage... <del>ipsum lorem bullshit</del>
+			</h3>
+			<hr />
+			<p>
+				lmao
+			</p>
+			<h4>
+				Other Platforms
+			</h4>
+			{/*Insert Social Media link bullshit */}
+			<ul>
+				<li>Twitter</li>
+				<li>Discord</li>
+				<li>Your Mom</li>
+			</ul>
+		</div>
+	</header>
+</div>
+);
 }
 
 export default App;
